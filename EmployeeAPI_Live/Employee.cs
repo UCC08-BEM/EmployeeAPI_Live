977d3d4c7a7710c8902813e3326299695e824490
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-06-07 10:11:44 +0300

    Add project files.

---
+++ EmployeeAPI_Live/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAPI_Live
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string City { get; set; }
+    }
+}
