0d1c1c56d8cf4b735769d91df28e6cfce0d5e729
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-06-07 10:17:11 +0300

    Some changes

---
+++ EmployeeAPI_Live/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI_Live.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // Öncelikle manuel bir şekilde bir liste yapısı oluşrup CRUD işlemlerini bunun üzerinde gerçekleştirelim.
+
+        private static List<Employee> employees = new List<Employee>
+        {
+                new Employee { Id = 1,FName="Ümit",LName="Karaçivi",City="İstanbul"},
+                new Employee { Id = 2,FName="Nurgül",LName="Karaçivi",City="Bursa"},
+                new Employee { Id = 3,FName="Doğa Bengi",LName="Karaçivi",City="İstanbul"}
+        };
+    }
+}
